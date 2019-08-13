aba734bd822fea003f0fdf831f8d844b4420ac7f
Mehrdad <mehrdad@mehrdadyou.telus>
2019-08-13 02:51:46 -0700

    fixed the successful login nock

--- test/eloqua.spec.js
+++ test/eloqua.spec.js
@@ -16,17 +16,46 @@ describe('test login method', function () {
         });
     });
     describe("with good credentials", function () {
+        var expectedResponse;
         before(function () {
             eloqua = new index_1.Eloqua("good-site", "good-user", "good-password");
+            expectedResponse = {
+                "site": {
+                    "id": 1,
+                    "name": "good-site"
+                },
+                "user": {
+                    "id": 2,
+                    "username": "good-user",
+                    "displayName": "good-user",
+                    "firstName": "good",
+                    "lastName": "user",
+                    "emailAddress": "good-user@good-site.com"
+                },
+                "urls": {
+                    "base": "https://secure.p01.eloqua.com",
+                    "apis": {
+                        "soap": {
+                            "standard": "https://secure.p01.eloqua.com/API/{version}/Service.svc",
+                            "dataTransfer": "https://secure.p01.eloqua.com/API/{version}/DataTransferService.svc",
+                            "email": "https://secure.p01.eloqua.com/API/{version}/EmailService.svc",
+                            "externalAction": "https://secure.p01.eloqua.com/API/{version}/ExternalActionService.svc"
+                        },
+                        "rest": {
+                            "standard": "https://secure.p01.eloqua.com/API/REST/{version}/",
+                            "bulk": "https://secure.p01.eloqua.com/API/Bulk/{version}/"
+                        }
+                    }
+                }
+            };
         });
         it('login successfully', function () {
             var url = new URL(index_1.Eloqua.loginUrl);
-            console.log(url);
             nock(url.origin)
                 .get(url.pathname)
-                .reply(200, "hello");
+                .reply(200, expectedResponse);
             return eloqua.login().then(function (body) {
-                chai_1.expect(body).eq("hello");
+                chai_1.expect(body).eq(JSON.stringify(expectedResponse));
             });
         });
     });
--- test/eloqua.spec.ts
+++ test/eloqua.spec.ts
@@ -16,18 +16,42 @@ describe('test login method', () => {
   });
 
   describe("with good credentials", () => {
+    let expectedResponse: object;
     before(() => {
       eloqua = new Eloqua("good-site", "good-user", "good-password");
+      expectedResponse = { 
+        "site": { 
+          "id": 1, 
+          "name": "good-site" }, 
+          "user": { 
+            "id": 2, 
+            "username": "good-user", 
+            "displayName": "good-user", 
+            "firstName": "good", 
+            "lastName": "user", 
+            "emailAddress": "good-user@good-site.com" 
+          }, 
+          "urls": { 
+            "base": "https://secure.p01.eloqua.com", 
+            "apis": { 
+              "soap": { 
+                "standard": "https://secure.p01.eloqua.com/API/{version}/Service.svc", 
+                "dataTransfer": "https://secure.p01.eloqua.com/API/{version}/DataTransferService.svc", 
+                "email": "https://secure.p01.eloqua.com/API/{version}/EmailService.svc", 
+                "externalAction": "https://secure.p01.eloqua.com/API/{version}/ExternalActionService.svc" 
+              }, 
+              "rest": { 
+                "standard": "https://secure.p01.eloqua.com/API/REST/{version}/", 
+                "bulk": "https://secure.p01.eloqua.com/API/Bulk/{version}/" } } } };
     });
     it('login successfully', () => {
       const url = new URL(Eloqua.loginUrl);
-      console.log(url);
       nock(url.origin)
         .get(url.pathname)
-        .reply(200, "hello");
+        .reply(200, expectedResponse);
 
       return eloqua.login().then(body => {
-        expect(body).eq("hello");
+        expect(body).eq(JSON.stringify(expectedResponse));
       });
     });
   });
